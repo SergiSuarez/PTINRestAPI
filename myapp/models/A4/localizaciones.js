8982d6e22d7bb4d1f8343c2046661e05fed3e326
SergiSuarez <40240086+SergiSuarez@users.noreply.github.com>
2020-05-20 15:00:41 +0200

    renombrar datos nodos

--- myapp/models/A4/localizaciones.js
+++ myapp/models/A4/localizaciones.js
@@ -2,10 +2,9 @@ var mongoose = require('mongoose'),
     Schema   = mongoose.Schema;
  
 var NodoSchema = new Schema({
-    nodo: String, //identificador del noso
+    nombre_nodo: String, //identificador del noso
     coordenadas: String,
-    nodo: String,
-    vecinos: String
+    nodos_vecinos: String
  },{collection:'nodos'});
 
 module.exports = mongoose.model('nodos', NodoSchema);
